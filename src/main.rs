e35ac33ea6d9f1f1f246beaf399e23618640438b
raffimolero <raffimolero@gmail.com>
2021-12-15 13:11:53 +0800

    controls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::TAU;
+
 use macroquad::prelude::*;
 
 use ergo::*;
@@ -101,26 +103,62 @@ async fn main() {
     let gl = unsafe { get_internal_gl().quad_gl };
     // camera for canvases
     let cam = &mut Camera2D::default();
+
     set_camera(cam);
 
     // initialize canvases
     let outer = new_canvas(256, 256);
-    let inner = new_canvas(64, 64);
+    let [mut x, mut y, mut yaw, mut pitch, mut roll] = [0.0; 5];
+    let mut zoom = 1.0;
+    let inner = new_canvas(256, 256);
 
     // main loop
     loop {
-        if let Some(key) = get_last_key_pressed() {
-            match key {
-                KeyCode::Q => {
-                    if is_key_down(KeyCode::LeftControl) {
-                        return;
-                    }
-                }
-                _ => {}
-            }
-        }
         // nearly every macroquad function uses f32 instead of f64 because that's what `Mat4`s are made of
         let time = get_time() as f32;
+        // for some reason this uses f32s already
+        let delta = get_frame_time();
+
+        // check mouse position
+        // mouse goes downwards, while transforms go upwards
+        let mouse = mouse_position_local();
+        {
+            let sensitivity = 0.5;
+            yaw = (mouse.x - x) * sensitivity;
+            pitch = (-mouse.y - y) * sensitivity;
+        }
+
+        // check keypresses
+        {
+            use KeyCode::*;
+            // Quit on Esc
+            if let Some(Escape) = get_last_key_pressed() {
+                return;
+            }
+
+            // WASD movement
+            if is_key_down(S) {
+                y -= delta;
+            }
+            if is_key_down(W) {
+                y += delta;
+            }
+            if is_key_down(A) {
+                x -= delta;
+            }
+            if is_key_down(D) {
+                x += delta;
+            }
+
+            // roll is stored clockwise, transforms go counterclockwise
+            let sensitivity = 0.5;
+            if is_key_down(Q) {
+                roll -= delta * sensitivity;
+            }
+            if is_key_down(E) {
+                roll += delta * sensitivity;
+            }
+        }
 
         // outermost layer just has a gray background
         clear_background(DARKBLUE);
@@ -128,8 +166,9 @@ async fn main() {
             clear_background(DARKGREEN);
             paint_canvas(inner, cam, |_| {
                 clear_background(DARKBROWN);
-                apply_transforms(gl, &[shift(-0.5, 0.0), flip_y(), downscale(256.0)], |_gl| {
-                    draw_text("sample text", 0.0, 0.0, 64.0, YELLOW);
+                let text_scale = 256.0;
+                apply_transforms(gl, &[downscale(text_scale), flip_y()], |_gl| {
+                    draw_text("sample text", text_scale * -0.5, 0.0, 64.0, ORANGE);
                 });
                 apply_transform(gl, rotate_turns(time / 3.0), |_| {
                     draw_line(0.0, 0.0, 0.0, 1.0, 0.25 * 0.25, BLUE);
@@ -144,9 +183,36 @@ async fn main() {
             apply_transforms(gl, &[rotation, translation], |gl| draw_canvas(inner, gl));
             apply_transforms(gl, &[translation, rotation], |gl| draw_canvas(inner, gl));
             // comment one out to find out which is which
+
+            let text_scale = 512.0;
+            apply_transforms(gl, &[downscale(text_scale), flip_y()], |_gl| {
+                draw_text(
+                    &format!("X: {}", mouse.x),
+                    text_scale * -0.375,
+                    text_scale * 0.125,
+                    64.0,
+                    YELLOW,
+                );
+                draw_text(
+                    &format!("Y: {}", mouse.y),
+                    text_scale * -0.375,
+                    text_scale * -0.125,
+                    64.0,
+                    YELLOW,
+                );
+            });
         });
         // draw rotating outer layer
-        apply_transform(gl, nyoom(time / 8.0), |gl| draw_canvas(outer, gl));
+        apply_transform(
+            gl,
+            Mat4::from_scale_rotation_translation(
+                vec3(zoom, zoom, 1.0),
+                // I'll be honest, I don't understand how yaw, pitch, and roll work.
+                Quat::from_rotation_ypr(yaw * TAU, -pitch * TAU, -roll * TAU),
+                vec3(x, y, 0.0),
+            ),
+            |gl| draw_canvas(outer, gl),
+        );
 
         // end frame
         next_frame().await
